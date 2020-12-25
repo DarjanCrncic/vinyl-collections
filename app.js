210d11c37db001eef2951acbadadf7e2db83cdd1
DarjanCrncic <darjancr@gmail.com>
2020-12-25 02:18:53 +0100

    added node version i package.json

--- app.js
+++ app.js
@@ -277,9 +277,11 @@ app.get("/othersCollection/:sortingType", function(req,res){
   }
 });
 
-
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
+
+app.listen(port, function(){
+  console.log("Server running");
+});
