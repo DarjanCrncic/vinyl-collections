95f0887f91437ca1cbfb7d4c0850f75a7cea77b6
DarjanCrncic <darjancr@gmail.com>
2020-12-25 02:13:23 +0100

    second try

--- app.js
+++ app.js
@@ -278,6 +278,8 @@ app.get("/othersCollection/:sortingType", function(req,res){
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server running on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
